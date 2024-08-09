0f76fe1b575ed4eb869c33d6255c8a0ecda77c73
fredymarb <29468132+fredymarb@users.noreply.github.com>
2024-08-09 17:41:47 +0000

    work with erb to make custom letters for each participant

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,22 +1,23 @@
 require "csv"
+require "erb"
 require "google/apis/civicinfo_v2"
 
 def clean_zipcode(zip)
   zip.to_s.rjust(5, "0")[0..4]
 end
 
-def legislators_by_zipcode(zip)
+def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = File.read("civic_info.key").strip
 
   begin
-    legislators = civic_info.representative_info_by_address(
-      address: zip,
+    civic_info.representative_info_by_address(
+      address: zipcode,
       levels: "country",
       roles: %w[legislatorUpperBody legislatorLowerBody]
-    )
-    legislators = legislators.officials
-    legislators.map(&:name).join(", ")
+    ).officials
+    # legislators = legislators.officials
+    # legislators.map(&:name).join(", ")
   rescue StandardError
     "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
@@ -30,10 +31,15 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read("form_letter.erb")
+erb_template = ERB.new(template_letter)
+
 contents.each do |row|
+  id = row[0]
   first_name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{first_name}: #{zipcode} (#{legislators})"
+  form_letter = erb_template.result(binding)
+  puts form_letter
 end
