a75064de4248d809c39a3b231c4b946258d6de97
fredymarb <29468132+fredymarb@users.noreply.github.com>
2024-08-09 17:47:29 +0000

    output form letters to a directory

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -41,5 +41,11 @@ contents.each do |row|
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
-  puts form_letter
+  Dir.mkdir("output") unless Dir.exist?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, "w") do |file|
+    file.puts form_letter
+  end
 end
