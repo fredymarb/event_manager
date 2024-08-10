c1fb1ac9e2c47c327fcea53cd8569d4ea4c7dcbb
fredymarb <29468132+fredymarb@users.noreply.github.com>
2024-08-10 13:04:01 +0000

    add phone numbers from csv and format it to a 10 digit format

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -16,11 +16,17 @@ def legislators_by_zipcode(zipcode)
       levels: "country",
       roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-    # legislators = legislators.officials
-    # legislators.map(&:name).join(", ")
   rescue StandardError
     "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
   end
+end
+
+def clean_phone_number(phone_number)
+  formatted_number = phone_number.gsub(/[^\d]/, "")
+  return formatted_number if formatted_number.length == 10
+  return formatted_number[1..10] if formatted_number.length == 11 && formatted_number[0] == "1"
+
+  "Invalid phone number"
 end
 
 def save_thank_you_letter(id, form_letter)
@@ -45,10 +51,14 @@ template_letter = File.read("form_letter.erb")
 erb_template = ERB.new(template_letter)
 
 contents.each do |row|
+  # "first_name" and "legislators" are called in the "erb_template"
   id = row[0]
   first_name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+
+  # phone_number has been formatted below
+  # phone_number = clean_phone_number(row[:homephone])
 
   form_letter = erb_template.result(binding)
   save_thank_you_letter(id, form_letter)
